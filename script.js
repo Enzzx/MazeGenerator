08207344816d36c25182771c54939c4eeaaa544f
Enzzx <101784007+Enzzx@users.noreply.github.com>
2024-04-25 15:14:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
+let finished = false
 let xDrop = 0
 let yDrop = 0
 let iI = 0
 let iJ = 0
-const squareSize = 50
+const perimeter = 20
+const squareSize = 450/perimeter
 const matrix = []
 const stack = []
 let cache = []
+let direction = []
 
 function setup() {
-  frameRate(1)
+  //frameRate(1)
   
   createCanvas(450, 450);
   background(0);
-  iI = int(random(9))
-  iJ = int(random(9))
-  
-  for (let i = 0; i < 9; i++) {
+
+  for (let i = 0; i < perimeter; i++) {
     const coluna = []
     matrix.push(coluna)
-    for (let j = 0; j < 9; j++) {
+    for (let j = 0; j < perimeter; j++) {
       const celula = new cell(xDrop, yDrop, i, j)
       //rect(xDrop, yDrop, squareSize, squareSize)
       coluna.push(celula)
-      xDrop+=50
+      xDrop+=squareSize
     }
     xDrop = 0
-    yDrop+=50
+    yDrop+=squareSize;
   }
   
+  [iI, iJ] = randomize();
   matrix[iI][iJ].color = "white"
-  matrix[iI][iJ].iterated = true
-  iI = int(random(9))
-  iJ = int(random(9))
+  matrix[iI][iJ].iterated = true;
+
+  [iI, iJ] = randomize();
+  matrix[iI][iJ].color = "grey"
+  cache.push([iI, iJ])
   
   matrix.forEach(coluna => {
     coluna.forEach(cell => {
@@ -43,22 +47,33 @@ function setup() {
 
 
 function draw() {
-  attCordinates()
-  /*
-  if (matrix[iI][iJ].color == "grey") {
-    matrix[iI][iJ].color = "white"
-  } else {
+  if (!finished) {
+    if (cache.length == 0) {
+      [iI, iJ] = randomize()
+      direction = []
+    }
+    attCordinates()
+    
     matrix[iI][iJ].color = "grey"
-  }*/
-  matrix[iI][iJ].color = "grey"
-  matrix[iI][iJ].iterated = true
-  
-  matrix.forEach(coluna => {
-    coluna.forEach(cell => {
-      cell.paint()
-      cell.walls()
+    
+    if (matrix[iI][iJ].iterated == true) {
+      cache.forEach(subarray => {
+        matrix[subarray[0]][subarray[1]].iterated = true
+        matrix[subarray[0]][subarray[1]].color = "white"
+      })
+      cache.length = 0
+    }
+    
+    matrix.forEach(coluna => {
+      coluna.forEach(cell => {
+        cell.paint()
+        cell.walls()
+      })
     })
-  })
+    
+  } else {
+    
+  }
 }
 
 
@@ -80,7 +95,7 @@ class cell {
     noStroke()
     fill(this.color)
     rect(this.x, this.y, squareSize, squareSize)
-    text(`${this.i}, ${this.j}`, this.x, this.y)
+    //text(`${this.i}, ${this.j}`, this.x, this.y)
   }
   
   walls() {
@@ -103,6 +118,8 @@ class cell {
 
 
 function attCordinates() {
+  //console.log(direction)
+  
   const upSideDown = random([0, 1])
   const plusMinus = random([-1, 1])
   const oldCoords = [iI, iJ]
@@ -125,30 +142,49 @@ function attCordinates() {
     })
     return
   }
+    
+    // if (direction[direction.length-1] == "t" && upSideDown == 0 && plusMinus == 1) {
+    //   console.log("voltou pro de baixo")
+    // } else if (direction == "b" && upSideDown == 0 && plusMinus == -1) {
+    //   console.log("voltou pro de cima")
+    // } else if (direction == "l" && upSideDown == 1 && plusMinus == 1) {
+    //   console.log("voltou pra direita")
+    // } else if (direction == "r" && upSideDown == 1 && plusMinus == -1) {
+    //   console.log("voltou pra esquerda")
+    // }
 
-  if ((iI < 0 || iI > 8) || (iJ < 0 || iJ > 8)) {
+  if ((iI < 0 || iI > perimeter-1) || (iJ < 0 || iJ > perimeter-1)) {
+    console.log("entrou")
     iI = oldCoords[0]
     iJ = oldCoords[1]
     return attCordinates()
   } else {
+    /*if (!matchArray(cache, [iI, iJ])) {
+      cache.push([iI, iJ])
+    }*/
+    
     if (upSideDown == 0 && plusMinus == -1) {
-      //left
-      matrix[oldCoords[0]][oldCoords[1]].leftW = false
-      matrix[iI][iJ].rightW = false
-    } else if (upSideDown == 0 && plusMinus == 1) {
-      //right
-      matrix[oldCoords[0]][oldCoords[1]].rightW = false
-      matrix[iI][iJ].leftW = false
-    } else if (upSideDown == 1 && plusMinus == -1) {
       //top
       matrix[oldCoords[0]][oldCoords[1]].topW = false
       matrix[iI][iJ].bottomW = false
-    } else {
+      //direction.push("t")
+    } else if (upSideDown == 0 && plusMinus == 1) {
       //bottom
       matrix[oldCoords[0]][oldCoords[1]].bottomW = false
       matrix[iI][iJ].topW = false
+      //direction.push("b")
+    } else if (upSideDown == 1 && plusMinus == -1) {
+      //left
+      matrix[oldCoords[0]][oldCoords[1]].leftW = false
+      matrix[iI][iJ].rightW = false
+      //direction.push("l")
+    } else {
+      //right
+      matrix[oldCoords[0]][oldCoords[1]].rightW = false
+      matrix[iI][iJ].leftW = false
+      //direction.push("r")
     }
-    cache.push(Array.of(iI, iJ))
+    cache.push([iI, iJ])
   }
 }
   
@@ -164,4 +200,13 @@ function matchArray(bid, uni) {
   return bid.some(subarray => {
     return subarray.every((valor, index) => valor === uni[index])
   })
+}
+  
+function randomize() {
+  let [a, b] = [int(random(perimeter)), int(random(perimeter))]
+  if (matrix[a][b].iterated) {
+    return randomize()
+  } else {
+    return [a, b];
+  }
 }
